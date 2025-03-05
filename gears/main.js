176eca73f4980acfce6ee095d70801fe8f478e04
liledix4 <liledix4@musician.org>
2025-03-05 03:44:36 +0000

    0.1.5.15

---
+++ gears/main.js
@@ -0,0 +1,29 @@
+import { readTextFile } from "../src/js/modules/js_xhr_ajax/xhr_ajax.js";
+
+
+readTextFile({url: 'https://bvsgame.github.io/data/version'}, fileContent => {
+  const num = parseInt(fileContent.match(/\d+$/)[0]);
+  document.getElementById('ver').innerText = num;
+  let i = 0;
+  function addStar() {
+    setTimeout(() => {
+      const top = Math.random() * 100;
+      const left = Math.random() * 100;
+
+      const star = document.createElement('div');
+
+      const starWrapper = document.getElementById('stars');
+      starWrapper.appendChild(star);
+
+      star.classList.add('star');
+      star.style.top = `${top}%`;
+      star.style.left = `${left}%`;
+
+      if (i < num) {
+        addStar();
+        i++;
+      }
+    }, 500 * Math.random());
+  }
+  addStar();
+});
