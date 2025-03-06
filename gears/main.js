8f7079e589361e49bf265b6980ffc2aa99688e02
liledix4 <liledix4@musician.org>
2025-03-06 00:11:07 +0000

    0.1.5.15.5

--- gears/main.js
+++ gears/main.js
@@ -2,28 +2,38 @@ import { readTextFile } from "../src/js/modules/js_xhr_ajax/xhr_ajax.js";
 
 
 readTextFile({url: 'https://bvsgame.github.io/data/version'}, fileContent => {
-  const num = parseInt(fileContent.match(/\d+$/)[0]);
-  document.getElementById('ver').innerText = num;
-  let i = 0;
-  function addStar() {
-    setTimeout(() => {
-      if (i < num) {
-        const top = Math.random() * 100;
-        const left = Math.random() * 100;
-
-        const star = document.createElement('div');
-
-        const starWrapper = document.getElementById('stars');
-        starWrapper.appendChild(star);
-
-        star.classList.add('star');
-        star.style.top = `${top}%`;
-        star.style.left = `${left}%`;
-
-        addStar();
-        i++;
-      }
-    }, 500 * Math.random());
-  }
-  addStar();
+  window.addEventListener('load', () => {
+    const num = parseInt(fileContent.match(/\d+$/)[0]);
+    fire(num);
+  });
 });
+
+
+function fire(versionNumber) {
+  document.getElementById('ver').innerText = versionNumber;
+  document.querySelector('body').classList.remove('loading');
+  addStar(versionNumber);
+}
+
+
+let curentStar = 0;
+function addStar(amountOfStars) {
+  setTimeout(() => {
+    if (curentStar < amountOfStars) {
+      const top = Math.random() * 100;
+      const left = Math.random() * 100;
+
+      const star = document.createElement('div');
+
+      const starWrapper = document.getElementById('stars');
+      starWrapper.appendChild(star);
+
+      star.classList.add('star');
+      star.style.top = `${top}%`;
+      star.style.left = `${left}%`;
+
+      addStar(amountOfStars);
+      curentStar++;
+    }
+  }, 500 * Math.random());
+}
